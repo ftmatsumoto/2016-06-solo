fc9e23174c45f0cea0a925f7262db21f7e1f307b
FELIPE MATSUMOTO <felipetmatsumoto@gmail.com>
2016-07-11 18:02:02 -0300

    fixed index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-var app = require('server/server.js');
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 
 var port = process.env.PORT || 3000;
 
