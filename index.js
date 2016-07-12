bcfd7074f3173fac2bd234503b6d9ea14bf04d34
FELIPE MATSUMOTO <felipetmatsumoto@gmail.com>
2016-07-12 01:21:31 -0300

    input form done

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
 var app = express();
 
+var port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/client'));
+//Store all HTML files in view folder.
+//Store all JS and CSS in Scripts folder.
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  console.log(req);
+  res.sendFile('index.html');
 });
 
-var port = process.env.PORT || 3000;
+app.get('/api/stockoption', function (req, res) {
+  console.log('22222222222');
+  res.sendFile('index.html');
+});
 
 app.listen(port);
 
